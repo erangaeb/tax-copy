b72654dca1b4bcf4c245509d296154355d17255b
eranga herath <erangaeb@gmail.com>
2016-07-01 17:17:26 +0530

    InputReader actor

---
+++ src/main/scala/com/pagero/taxcopy/actors/InputReader.scala
@@ -0,0 +1,50 @@
+package com.pagero.taxcopy.actors
+
+import akka.actor.SupervisorStrategy.{Restart, Stop}
+import akka.actor.{Actor, OneForOneStrategy, Props}
+import org.slf4j.LoggerFactory
+
+object InputReader {
+
+  case class InitReader()
+
+  def props(): Props = Props(new InputReader())
+}
+
+class InputReader extends Actor {
+
+  import InputReader._
+
+  def logger = LoggerFactory.getLogger(this.getClass)
+
+  override def preStart() = {
+    logger.info("[_________START ACTOR__________] " + context.self.path)
+  }
+
+  override def supervisorStrategy = OneForOneStrategy() {
+    case e: NullPointerException =>
+      logger.error("Null pointer exception caught [RESTART]" + e)
+      Restart
+    case e: Exception =>
+      logger.error("Exception caught, [STOP] " + e)
+      Stop
+  }
+
+  override def receive: Receive = {
+    case InitReader =>
+      // listen for user inputs form commandline
+      println()
+      println()
+      println("---------------------------------------")
+      println("ENTER PATH[/Users/eranga/workspace/dir]")
+      println("---------------------------------------")
+      println()
+
+      // read user input from the command line
+      val input = scala.io.StdIn.readLine()
+
+      logger.debug("Input : " + input)
+
+    // context.actorOf(shareHandlerComp.ShareHandler.props(inputSenz))
+  }
+}
---
+++ src/main/scala/com/pagero/taxcopy/boot/Main.scala
@@ -0,0 +1,22 @@
+package com.pagero.taxcopy.boot
+
+import akka.actor.ActorSystem
+import com.pagero.taxcopy.actors.InputReader
+import com.pagero.taxcopy.actors.InputReader.InitReader
+import org.slf4j.LoggerFactory
+
+/**
+ * Created by eranga on 1/9/16.
+ */
+object Main extends App {
+
+  def logger = LoggerFactory.getLogger(this.getClass)
+
+  logger.debug("Booting application")
+
+  implicit val system = ActorSystem("tax-copy")
+
+  // start senz listener
+  val inputReader = system.actorOf(InputReader.props(), name = "InputReader")
+  inputReader ! InitReader
+}
