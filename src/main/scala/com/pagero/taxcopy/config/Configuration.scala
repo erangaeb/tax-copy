a014fc35180bc61fde8b3cf46db364b205ff31b8
eranga herath <erangaeb@gmail.com>
2016-07-04 12:06:20 +0530

    cassandra config

--- src/main/scala/com/pagero/taxcopy/config/Configuration.scala
+++ src/main/scala/com/pagero/taxcopy/config/Configuration.scala
@@ -13,7 +13,7 @@ trait Configuration {
   // config object
   val config = ConfigFactory.load()
 
-  // dir config
-  lazy val imgDirName = Try(config.getString("dir.img.name")).getOrElse("home")
-  lazy val pdfDirName = Try(config.getString("dir.pdf.name")).getOrElse("home")
+  // cassandra db config
+  lazy val cassandraHost = Try(config.getString("db.cassandra.host")).getOrElse("localhost")
+  lazy val cassandraPort = Try(config.getInt("db.cassandra.port")).getOrElse(9160)
 }
